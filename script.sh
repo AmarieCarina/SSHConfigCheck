9dc6c1a0a00eba2834b88fed0e7c0a4e61ee2305
Amarie Carina <amarieelenacarina@gmail.com>
2026-01-08 19:17:43 +0200

    Verificare permisiuni

--- script.sh
+++ script.sh
@@ -11,7 +11,49 @@ fi
 file="$1"
 
 #Verificare existenta fisier
+echo "Verifying file existance: "
 if [ ! -f "$file" ]; then
 	echo "ERROR: No configuration file provided."
+	exit 1
+else
+	echo "File exists."
+fi
+echo
+
+#Verificare permisiuni
+echo "Verifying file permissions: "
+if [ -x "$file" ]; then
+	echo "All good, executable file."
+else
+	echo "Warning! Unexecutable file."
+	exit 1
+fi
+
+if [ -r "$file" ]; then
+	echo "All good, readable file."
+else
+	echo "Warning! Unreadable file."
+	exit 1
+fi
+
+if [ -w "$file" ]; then
+	echo "All good, writable file."
+else
+	echo "Warning! Unwritable file."
+fi
+echo
+
+perm=$(stat -c "%a" "$file")
+others=${perm: -1}
+if (( others & 4 )); then
+	echo "Warning! Others can read the file."
+	exit 1
+fi
+if (( others & 2 )); then
+	echo "Warning! Others can write to the file."
+	exit 1
+fi
+if (( others & 1 )); then
+	echo "Warning! Others can execute the file."
 	exit 1
 fi
